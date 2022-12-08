1c9637f0f711e1b30f2d53e08958671e4e3b5f90
30844185 <danielhika05@gmail.com>
2022-12-08 10:14:38 -0800

    My first c programming day

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/**
- *main - Entry point
+/*
+ * main - print the code using puts
  *
- *Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
